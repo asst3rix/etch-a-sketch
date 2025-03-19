c091dc955548a326a7aed846e7907f7cf9174895
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-03-19 17:00:59 +0100

    Add EventListener when mouseovering a square. The EventListener adds a CSS class.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector("#container");
 
+// Creation of the 16x16 grid
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     container.appendChild(div);
 }
+
+// We select all the squares
+const squares = document.querySelectorAll("#container div");
+// We affect to all the squares, a mouseover EventListener that add a CSS class
+squares.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.classList.add("mouseover");
+    });
+});
