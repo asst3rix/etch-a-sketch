79513b1e5992ecb18882d06f4d92c1d7a9226e07
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-03-20 11:48:43 +0100

    Rename #container div as #grid div.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const container = document.querySelector("#container");
+const grid = document.querySelector("#grid");
 
 // Creation of the 16x16 grid
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    container.appendChild(div);
+    grid.appendChild(div);
 }
 
 // We select all the squares
-const squares = document.querySelectorAll("#container div");
+const squares = document.querySelectorAll("#grid div");
 // We affect to all the squares, a mouseover EventListener that add a CSS class
 squares.forEach((div) => {
     div.addEventListener("mouseover", () => {
