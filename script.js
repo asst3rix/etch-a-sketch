ca6808f67d3591a6de8b1f678054a42ed3417fa6
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-03-20 13:43:20 +0100

    Add function createGrid(). Create a grid with a given size. Add function changeGridSize(). Change the size of  the grid with the value of the input type number.

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 const grid = document.querySelector("#grid");
+const gridSizeButton = document.querySelector("#gridSizeButton");
+const gridSize = document.querySelector("#gridSize");
 
 // Creation of the 16x16 grid
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    grid.appendChild(div);
-}
+createGrid(gridSize.value);
+
+gridSizeButton.addEventListener("click", () => { changeGridSize(gridSize.value); });
 
 // We select all the squares
 const squares = document.querySelectorAll("#grid div");
 // We affect to all the squares, a mouseover EventListener that add a CSS class
 squares.forEach((div) => {
-    div.addEventListener("mouseover", () => {
+    div.addEventListener("click", () => {
         div.classList.add("mouseover");
     });
 });
+
+function createGrid(size) {
+    for (let i = 0; i < size*size; i++) {
+        const div = document.createElement("div");
+        div.style.flex = "1 0 " + 100/size + "%";
+        grid.appendChild(div);
+    }
+}
+
+function changeGridSize(size) {
+    // We delete the actual grid
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    // To create it again
+    createGrid(size);
+}
